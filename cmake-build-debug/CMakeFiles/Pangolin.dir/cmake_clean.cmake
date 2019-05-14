3c5518266175a8658b24635b5badebec1911851d
Renny Lindberg <renny@wise58.vub.ac.be>
2019-05-14 15:43:54 +0200

    main created

---
+++ cmake-build-debug/CMakeFiles/Pangolin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pangolin.dir/main.cpp.o"
+  "Pangolin.pdb"
+  "Pangolin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pangolin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
